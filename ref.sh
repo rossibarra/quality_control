f65c480e12d13cb945eb324bf02c3d2a5ff58618
rossibarra <rossibarra@ucdavis.edu>
2015-06-18 09:35:38 -0700

    indexing working, testing qc

--- ref.sh
+++ ref.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -l
-#SBATCH -o out/
-#SBATCH -e error/
+#SBATCH -o out/out-%j.txt
+#SBATCH -e error/error-%j.txt
 #SBATCH -J qc_ref
 set -e
 set -u
 
+module load bwa/0.7.9a
+
 # check for reference, if not exit
 if [ ! -f data/ref.fasta.gz ]; then
         echo "ERROR: no reference genome"
-        exit 1 # terminate and indicate error
+        exit 1 
 fi;
 
 #make index
